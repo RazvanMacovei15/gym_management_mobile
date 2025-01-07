a6954e6dad0c0ef9751891661d4f6ce7ba7dc56b
RazvanMacovei15 <razvanmc15@gmail.com>
2025-01-07 20:59:23 +0200

    wtf

--- gym_frontend/app/_layout.jsx
+++ gym_frontend/app/_layout.jsx
@@ -11,19 +11,16 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 const RootLayout = () => {
   return (
     <SafeAreaProvider>
-<AuthProvider>
-      <ModalProvider>
-        <Stack>
-          <Stack.Screen name="index" options={{ headerShown: false }} />
-          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        </Stack>
-      </ModalProvider>
-      
-    </AuthProvider>
+      <AuthProvider>
+        <ModalProvider>
+          <Stack>
+            <Stack.Screen name="index" options={{ headerShown: false }} />
+            <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          </Stack>
+        </ModalProvider>
+      </AuthProvider>
     </SafeAreaProvider>
-
-    
   );
 };
 
